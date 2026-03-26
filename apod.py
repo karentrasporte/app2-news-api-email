68e450609cf26e44aa25314b435734ee4ae9c18f
karentrasporte <karentrasporte@gmail.com>
2026-03-26 17:23:23 +0800

    Added code to create a web page with the APOD using NASA API.

--- apod.py
+++ apod.py
@@ -1,11 +1,19 @@
 import requests
+import streamlit as st
+import requests
 
 api_key = "rTwSX417C7S9aa2ZqLYNwCBlpVab9uofZZjNvBCM"
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
 response = requests.get(url)
-img_url = response.json()["url"]
-get_img = requests.get(img_url)
+img_url = requests.get(response.json()["url"])
+img_title = response.json()["title"]
+img_desc = response.json()["explanation"]
 
-with open("apod.jpg", "wb") as file:
-    file.write(get_img.content)
+
+with open("pic_of_the_day.jpg", "wb") as file:
+    file.write(img_url.content)
+
+st.title(img_title,text_alignment="center")
+st.image("pic_of_the_day.jpg", caption="Astronomy Picture of the Day")
+st.text(img_desc)
