79e055f7cb7bbea1bcc9e6e45751afe36640774b
karentrasporte <karentrasporte@gmail.com>
2026-03-26 13:58:02 +0800

    Parameterized topic, add TypeError in exception, changed email subject.

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import requests
 import send_email
 
 api_key="8c0f46f20ee44bad8d56878f054e2e97"
+topic="tesla"
 
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2026-02-26&sortBy=publishedAt&apiKey={api_key}"
+url = "https://newsapi.org/v2/everything?" \
+       f"q={topic}&" \
+       "from=2026-02-26&sortBy=publishedAt&" \
+       f"apiKey={api_key}&" \
+       "language=en"
 
 request = requests.get(url)
 content = request.json()
 
 email_message = ""
-
 for article in content["articles"]:
     try:
-        email_message = email_message + (article["title"]) + "\n" + (article["description"]) + "\n" + (article["url"] + "\n" + "\n")
-    except:
-        print(article["title"])
-        pass
-    # print(article["title"])
+        email_message = email_message + (article["title"]) + "\n" + (article["description"]) + "\n" + (article["url"] + 2* "\n")
+    except TypeError:
+       pass
+    #print(article["title"])
     # print(article["description"])
     # print(article["url"])
 
@@ -25,6 +28,6 @@ send_email.send_email(
     sender_email="traspo811@gmail.com",
     app_password="gludymxnthvmohff",
     recipient_email="traspo811@gmail.com",
-    subject="Test Email",
+    subject="Today's News!",
     body=str(email_message)
 )
